bae2ec84dbe8464f43250e7c5036d2e3a4149ad1
Spong3ibob <leonardomaran17@gmail.com>
2022-11-03 11:47:37 +0100

    creato numeri casuali

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let randInt = Math.floor(Math.random()*10);
+
+console.log(randInt);
+const boardContainer = document.querySelector(".container")
+
+for(let i = 1; i <= 100; i++){
+    const boardCell = document.createElement("div");
+    boardCell.innerHTML = i;
+    boardCell.classList.add("box");
+    boardCell.addEventListener("click", function() {
+        console.log(this.innerHTML)
+        this.classList.add("color")
+    })
+    boardContainer.append(boardCell);
+}
