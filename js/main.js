1d04c1946e05d6de27947e34ceafc4e300f22001
Spong3ibob <leonardomaran17@gmail.com>
2022-11-03 15:02:54 +0100

    problema

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
-
+const bombe = [];
 const boardContainer = document.querySelector(".container")
+const cella = document.querySelector(".box")
 
 for(let i = 1; i <= 100; i++){
     const boardCell = document.createElement("div");
     boardCell.innerHTML = i;
     boardCell.classList.add("box");
     boardCell.addEventListener("click", function() {
-        console.log(this.innerHTML)
         this.classList.add("color")
     })
     boardContainer.append(boardCell);
+
+    for(let i= 1; i <= 16; i++){
+        let randomNumber = Math.floor(Math.random()*100);
+        bombe.includes(randomNumber);
+        bombe.push(randomNumber);
+    }
+
+    boardCell.includes(randomNumber).addEventListener("click", function(){
+        cella.classList.add("colorsecondary")
+    })
 }
 
-const bombe = [];
-for(let i= 1; i <= 16; i++){
-    let randomNumber = Math.floor(Math.random()*100);
-    bombe.includes(randomNumber);
-    bombe.push(randomNumber);
-}
-console.log(bombe)
+
